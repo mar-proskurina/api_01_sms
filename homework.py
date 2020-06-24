9607bcceef94f15184899f921d85d4b42a24b259
mar-proskurina <mar.proskurina@gmail.com>
2020-06-24 10:20:30 +0300

    turned some variables to constants, added a function to parse first name of the user

--- homework.py
+++ homework.py
@@ -6,23 +6,22 @@ from dotenv import load_dotenv
 from twilio.rest import Client
 
 load_dotenv()
-vk_token = os.getenv('access_token')
+VK_TOKEN = os.getenv('access_token')
+URL_VK_API = 'https://api.vk.com/method/users.get'
+VK_API_VERSION = 5.92
 
 def get_status(user_id):
     params = {
         'user_ids': user_id,
-        'v': 5.92,
+        'v': VK_API_VERSION,
         'fields': 'online',
-        'access_token': vk_token
+        'access_token': VK_TOKEN
     }
-    user_status = requests.post(
-        url='https://api.vk.com/method/users.get', 
-        params=params
-    )
+    user_status = requests.post(url=URL_VK_API, params=params)
     return user_status.json()['response'][0]['online']
 
-caller = os.getenv('NUMBER_FROM')
-receiver = os.getenv('NUMBER_TO')
+CALLER = os.getenv('NUMBER_FROM')
+RECEIVER = os.getenv('NUMBER_TO')
 
 account_sid = os.getenv('account_sid')
 auth_token = os.getenv('auth_token')
@@ -31,15 +30,26 @@ client = Client(account_sid, auth_token)
 def sms_sender(sms_text):
     message = client.messages.create(
         body=sms_text,
-        from_=caller,
-        to=receiver
+        from_=CALLER,
+        to=RECEIVER
     )
     return message.sid
+
+def vk_user_name(user_id):
+    params = {
+                'user_ids': vk_id,
+                'v': VK_API_VERSION,
+                'fields': 'online',
+                'access_token': VK_TOKEN
+            }
+    first_name = requests.post(url=URL_VK_API, params=params)
+    return first_name.json()['response'][0]['first_name']
 
 if __name__ == '__main__':
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
-            sms_sender(f'Машуня {vk_id} сейчас онлайн! Машуня молодец!')
+            first_name = vk_user_name(vk_id)
+            sms_sender(f'{first_name} {vk_id} сейчас онлайн! Машуня молодец!')
             break
         time.sleep(5)
